99eec6ca320af4ac1968be011a306cd8b7a873fc
LEEJ0NGWAN <lastffx@gmail.com>
2025-04-15 14:50:53 +0900

    feat: main loop

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from bluetooth_handler import BullshitoothHandler
+from keyboard_handler import BullshitKeyboardHandler
+from mouse_handler import BullshitMouseHandler
+
+async def start_device_handlers():
+
+    asyncio.create_task(BullshitMouseHandler.start())
+
+    loop = asyncio.get_running_loop()
+
+    await loop.run_in_executor(None, BullshitKeyboardHandler.start)
+
+def main():
+
+    try:
+
+        BullshitoothHandler.start()
+        asyncio.run(start_device_handlers())
+
+    except KeyboardInterrupt: print()
+    except Exception: print()
+
+    finally:
+        BullshitoothHandler.stop()
+
+if __name__ == '__main__':
+    main()
