198095cc6bec45e7704ae352bbccb6191fbab519
LEEJ0NGWAN <lastffx@gmail.com>
2025-04-15 14:44:58 +0900

    feat: pynput keyboard code to hid usage id mapper

---
+++ keyboard_hid_usage_id_map.py
@@ -0,0 +1,25 @@
+import json
+
+KEYBOARD_HID_USAGE_ID_MAP_JSON = 'keyboard_hid_usage_id_map.json'
+
+class BullshitKeyboardMap:
+
+    map:dict = None
+
+    @classmethod
+    def load(cls):
+
+        with open(KEYBOARD_HID_USAGE_ID_MAP_JSON) as file:
+            cls.map = json.load(file)
+
+    @classmethod
+    def get(cls, key:str):
+
+        if cls.map is None: cls.load()
+
+        hid_usage_id = None
+
+        if key in cls.map:
+            hid_usage_id = cls.map.get(key)
+
+        return hid_usage_id
